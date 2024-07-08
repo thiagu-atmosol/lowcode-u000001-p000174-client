cfa233b9e087c725f2b9b6cc4517ad4c9502e750
thiagu-atmosol <thiagarajan@inncrewin.com>
2024-07-08 06:42:53 +0000

    Code updated for the deployment id:

---
+++ index.js
@@ -0,0 +1,135 @@
+// add more complex scripts as needed
+console.log("Hello from index.js!");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const driver = window.driver.js.driver
+    const driverObj = driver({
+        showProgress: true,
+        steps: [
+            
+            { element: '#section1221', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1222', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1224', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1235', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1225', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1223', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1226', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1237', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1247', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1265', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1266', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1248', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1227', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1238', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1249', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1268', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1269', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1250', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1228', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1239', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1251', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1271', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1272', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1252', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1229', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1240', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1253', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1274', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1275', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1254', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1230', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1241', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1255', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1277', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1278', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1256', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1231', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1242', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1257', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1280', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1281', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1258', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1232', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1243', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1259', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1283', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1284', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1260', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1233', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1244', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1261', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1286', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1287', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1262', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1234', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1245', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1263', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1289', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+            { element: '#section1290', popover: { title: 'static Section', description: 'This is the static section.', align: 'start' }},
+            
+            { element: '#section1264', popover: { title: 'container Section', description: 'This is the container section.', align: 'start' }},
+            
+        ]
+    });
+
+    // document.getElementById('start-tour').addEventListener('click', () => {
+        driverObj.drive();
+    //});
+});
